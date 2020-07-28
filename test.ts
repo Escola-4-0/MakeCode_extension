deb19c8fcdacbb1d99c4a98547a5d1bc9318432d
Samuel Fraga <36747917+samufraga@users.noreply.github.com>
2020-07-28 14:01:41 -0300

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,9 @@
 // tests go here; this will not be compiled when this package is used as a library
+Escola4ponto0.motorRun(EscolaMotorPick.MotorA, 50)
+Escola4ponto0.motorRun(EscolaMotorPick.MotorB, 50)
+basic.pause(3000)
+Escola4ponto0.motorStop(EscolaMotorPick.MotorA)
+Escola4ponto0.motorStop(EscolaMotorPick.MotorB)
+basic.pause(1000)
+Escola4ponto0.motorRunAB(40, 70, 2, EscolaMoveUnit.Rotations)
+Escola4ponto0.motorRunAB(50, 50, 3, EscolaMoveUnit.Seconds)
