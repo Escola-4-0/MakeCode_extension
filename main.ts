efae542b11a57c22e8741d6ae856aacef60113b9
Samuel Fraga <36747917+samufraga@users.noreply.github.com>
2020-07-27 23:06:03 -0300

    Giro limitado por tempo e giro limitado por rotações fundidos na função motorRun. Criada a função para girar A+B.

--- main.ts
+++ main.ts
@@ -46,13 +46,11 @@ class Motor {
         pins.setEvents(this.sensor2, PinEventType.Edge)
         pins.setPull(this.sensor1, PinPullMode.PullUp)
         pins.setPull(this.sensor2, PinPullMode.PullUp)
-        serial.writeLine("On")
     }
 
     encoderDisable(): void {
         pins.setEvents(this.sensor1, PinEventType.None)
         pins.setEvents(this.sensor2, PinEventType.None)
-        serial.writeLine("Off")
     }
 
     stepCounter(value:number): void{
@@ -62,15 +60,21 @@ class Motor {
         pins.setPull(this.sensor2, PinPullMode.PullUp)
         let state1=pins.digitalReadPin(this.sensor1)
         let state2=pins.digitalReadPin(this.sensor2)
-        while(counter<value){
+        while(true){
             read=pins.digitalReadPin(this.sensor1)
             if(state1!=read){
                 counter+=1
+                if(counter>=value){
+                    break
+                }
                 state1=read
             }
             read=pins.digitalReadPin(this.sensor2)
             if(state2!=read){
                 counter+=1
+                if(counter>=value){
+                    break
+                }
                 state2=read
             }
         }
@@ -85,10 +89,10 @@ enum MotorPick {
     MotorB
 }
 enum MoveUnit {
-    //% block="rotações"
-    Rotations,
     //% block="segundos"
-    Seconds
+    Seconds,
+    //% block="rotações"
+    Rotations
 }
 enum ServoDegrees {
     //%block="90°"
@@ -102,18 +106,18 @@ enum ServoDegrees {
 }
 
 //% color="#2695b5" weight=100 icon="\uf1b0" block="Escola 4.0"
-//% groups=['Motores', 'Servo Motor']
+//% groups=['Motores']
 namespace Escola4_0 {
     /**
-     * Gira o motor em uma dada velocidade por um tempo limitado (opcional). Se a velocidade for positiva,
-     * o motor gira em um sentido, se for negativa, o motor gira no sentido inverso
+     * Gira o motor em uma dada velocidade por determinado tempo ou quantidade de rotações.
+     * Se a velocidade for positiva, o motor gira em um sentido, se for negativa, o motor gira no sentido inverso.
      */
-    //% block="girar motor %motor com velocidade %speed\\% || por %value unit"
+    //% block="girar motor %motor com velocidade %speed\\% || por %duration %unit"
     //% group='Motores' weight=100 blockGap=8
-    //% expandableArgumentMode="enabled"    inlineInputMode=inline
+    //% expandableArgumentMode="toggle"    inlineInputMode=inline
     //% speed.shadow="speedPicker"
-    //% value.min=0
-    export function motorContinuous(motor: MotorPick, speed: number, value: number = 0) {
+    //% duration.min=0
+    export function motorRun(motor: MotorPick, speed: number, duration?: number, unit?: MoveUnit) {
         let motorTest = new Motor(motor)
         motorTest.speed(Math.abs(speed))
         if (speed > 0) {
@@ -121,27 +125,36 @@ namespace Escola4_0 {
         } else {
             motorTest.runReverse()
         }
-        if (value != 0) {
-            basic.pause(value * 1000)
+        if (duration) {
+            switch(unit) {
+                case MoveUnit.Seconds:
+                    basic.pause(duration * 1000)
+                    break;
+                case MoveUnit.Rotations:
+                    motorTest.stepCounter(duration*40)
+                    break;
+            }
             motorTest.stop()
         }
     }
 
     /**
-     * Gira os motores A e B ao mesmo tempo, com velocidades independentes por um tempo limitado (opcional). Se a velocidade for positiva,
-     * o motor gira em um sentido, se for negativa, o motor gira no sentido inverso
+     * Gira os motores A e B ao mesmo tempo, com velocidades independentes.
+     * Se definida, a quantidade de rotações é realizada pelo motor A.
      */
-    //% block="girar motores A+B com velocidades A:%speedA\\% e B:%speedB\\% || por %value segundos"
+    //% block="girar motores A+B com velocidades A:%speedA\\% e B:%speedB\\% || por %duration %unit"
     //% group='Motores' weight=90 blockGap=8
-    //% expandableArgumentMode="enabled"    inlineInputMode=inline
+    //% expandableArgumentMode="toggle"    inlineInputMode=inline
     //% speedA.shadow="speedPicker" speedB.shadow="speedPicker"
-    //% value.min=0
-    export function motorRunAB(speedA: number, speedB: number, value: number = 0) {
-        
+    //% duration.min=0
+    export function motorRunAB(speedA: number, speedB: number, duration?: number, unit?:MoveUnit) {
+        motorRun(MotorPick.MotorB, speedB)
+        motorRun(MotorPick.MotorA, speedA, duration, unit)
+        motorStop(MotorPick.MotorB)
     }
 
     /**
-     * Altera a velocidade do motor para um valor entre 0 e 100% (sem alterar o sentido de rotação)
+     * Altera a velocidade do motor para um valor entre 0 e 100%, sem alterar o sentido de rotação.
      */
     //% block="velocidade do motor %motor em %speed\\%"
     //% group='Motores' weight=50 blockGap=8
@@ -152,31 +165,12 @@ namespace Escola4_0 {
     }
 
     /**
-     * Interrompe a rotação do motor
+     * Interrompe a rotação do motor.
      */
     //% block="parar motor %motor"
     //% group='Motores' weight=0 blockGap=8
     export function motorStop(motor: MotorPick) {
         let motorTest = new Motor(motor)
-        motorTest.stop()
-    }
-
-    /**
-     * Gira o servo motor por uma quantidade limitada de rotações
-     */
-    //% block="girar servo motor %motor por %value rotações com velocidade %speed\\%"
-    //% group='Servo Motor' blockGap=8
-    //% expandableArgumentMode="toggle"     inlineInputMode=inline
-    //% speed.shadow="speedPicker"
-    export function motorRotations(motor: MotorPick, value: number = 0, speed: number) {
-        let motorTest = new Motor(motor)
-        motorTest.speed(Math.abs(speed))
-        if (speed > 0) {
-            motorTest.runDirect()
-        } else {
-            motorTest.runReverse()
-        }
-        motorTest.stepCounter(value*40)
         motorTest.stop()
     }
     
