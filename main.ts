b34eaff30f60f164c889cc4caf286715e8da1e5b
Samuel Fraga <36747917+samufraga@users.noreply.github.com>
2021-02-18 18:11:07 -0300

    Novos blocos para controlar o carrinho simples e separação dos blocos em dois grupos ("robô descontrolado" e "carrinho")

--- main.ts
+++ main.ts
@@ -5,6 +5,13 @@ enum EscolaMotorPick {
     MotorB
 }
 
+enum EscolaCarMotor{
+    //% block="E"
+    MotorE,
+    //% block="D"
+    MotorD
+}
+
 enum EscolaMoveUnit {
     //% block="segundos"
     Seconds,
@@ -25,7 +32,7 @@ enum EscolaPins{
 }
 
 //% color="#2695b5" weight=100 icon="\uf1b0" block="Escola 4.0"
-//% groups=['Motores']
+//% groups=['Robô desControlado', 'Carrinho']
 namespace Escola4ponto0 {
     let pcf_data=240
     let pcf_address=0
@@ -116,6 +123,7 @@ namespace Escola4ponto0 {
     /**
      * Configura o endereço do expansor i2c
      */
+    //% group='Robô desControlado'
     //% block="configurar %chip"
     //% weight=100 blockGap=10
     export function expanderAddress(chip: EscolaExpander){
@@ -125,9 +133,10 @@ namespace Escola4ponto0 {
     /**
      * Escreve valor digital (0 ou 1) em um pino do expansor i2c
      */
+    //% group='Robô desControlado'
     //% block="gravação digital pino %pin para %value"
     //% value.min=0 value.max=1
-    //% weight=50 blockGap=10
+    //% weight=20 blockGap=10
     export function expanderPinWrite(pin: EscolaPins, value:number){
         if(value){
             pcf_data = pcf_data|pin
@@ -140,8 +149,9 @@ namespace Escola4ponto0 {
     /**
      * Ler valor digital (0 ou 1) em um pino do expansor i2c
      */
+    //% group='Robô desControlado'
     //% block="leitura digital pino %pin"
-    //% weight=0 blockGap=10
+    //% weight=10 blockGap=10
     export function expanderPinRead(pin: EscolaPins): number {
         return (pin&pins.i2cReadNumber(pcf_address, NumberFormat.Int8LE, false))/pin;
     }
@@ -151,7 +161,7 @@ namespace Escola4ponto0 {
      * Se a velocidade for positiva, o motor gira em um sentido, se for negativa, o motor gira no sentido inverso.
      */
     //% block="girar motor %motor com velocidade %speed\\% || por %duration %unit"
-    //% group='Motores' weight=100 blockGap=8
+    //% group='Robô desControlado' weight=90 blockGap=8
     //% expandableArgumentMode="toggle"    inlineInputMode=inline
     //% speed.shadow="speedPicker"
     //% duration.min=0
@@ -181,7 +191,7 @@ namespace Escola4ponto0 {
      * Se definida, a quantidade de rotações é realizada pelo motor A.
      */
     //% block="girar motores A+B com velocidades A:%speedA\\% e B:%speedB\\% || por %duration %unit"
-    //% group='Motores' weight=90 blockGap=8
+    //% group='Robô desControlado' weight=80 blockGap=8
     //% expandableArgumentMode="toggle"    inlineInputMode=inline
     //% speedA.shadow="speedPicker" speedB.shadow="speedPicker"
     //% duration.min=0
@@ -197,7 +207,7 @@ namespace Escola4ponto0 {
      * Altera a velocidade do motor para um valor entre 0 e 100%, sem alterar o sentido de rotação.
      */
     //% block="velocidade do motor %motor em %speed\\%"
-    //% group='Motores' weight=50 blockGap=8
+    //% group='Robô desControlado' weight=50 blockGap=8
     //% speed.min=0 speed.max=100
     export function motorSpeed(motor: EscolaMotorPick, speed: number) {
         let myMotor = new Motor(motor)
@@ -208,9 +218,75 @@ namespace Escola4ponto0 {
      * Interrompe a rotação do motor.
      */
     //% block="parar motor %motor"
-    //% group='Motores' weight=0 blockGap=8
+    //% group='Robô desControlado' weight=40 blockGap=8
     export function motorStop(motor: EscolaMotorPick) {
         let myMotor = new Motor(motor)
         myMotor.stop()
+    }
+
+
+
+
+    /**
+     * Gira o motor em uma dada velocidade por determinado tempo.
+     * Se a velocidade for positiva, o motor gira em um sentido, se for negativa, o motor gira no sentido inverso.
+     */
+    //% block="girar motor %motor com velocidade %speed\\%"
+    //% group='Carrinho' weight=100 blockGap=8
+    //% expandableArgumentMode="toggle"    inlineInputMode=inline
+    //% speed.shadow="speedPicker"
+    export function carMotorRun(motor: EscolaCarMotor, speed: number) {
+        if (motor==EscolaCarMotor.MotorD){
+            if (speed > 0) {
+                pins.digitalWritePin(DigitalPin.P0, 0)
+                pins.digitalWritePin(DigitalPin.P1, 1)
+                pins.analogWritePin(AnalogPin.P8, 10*speed)
+            } else {
+                pins.digitalWritePin(DigitalPin.P0, 1)
+                pins.digitalWritePin(DigitalPin.P1, 0)
+                pins.analogWritePin(AnalogPin.P8, -10*speed)
+            }
+        }else{
+            if (speed>0) {
+                pins.digitalWritePin(DigitalPin.P2, 0)
+                pins.digitalWritePin(DigitalPin.P13, 1)
+                pins.analogWritePin(AnalogPin.P15, 10*speed)
+            }else{
+                pins.digitalWritePin(DigitalPin.P2, 1)
+                pins.digitalWritePin(DigitalPin.P13, 0)
+                pins.analogWritePin(AnalogPin.P15, -10*speed)
+            }                
+        }
+    }
+
+    /**
+     * Altera a velocidade do motor para um valor entre 0 e 100%, sem alterar o sentido de rotação.
+     */
+    //% block="velocidade do motor %motor em %speed\\%"
+    //% group='Carrinho' weight=50 blockGap=8
+    //% speed.min=0 speed.max=100
+    export function carMotorSpeed(motor: EscolaCarMotor, speed: number) {
+        if(motor==EscolaCarMotor.MotorD){
+            pins.analogWritePin(AnalogPin.P8, 10*speed)
+        }else{
+            pins.analogWritePin(AnalogPin.P15, 10*speed)
+        }
+    }
+
+    /**
+     * Interrompe a rotação do motor.
+     */
+    //% block="parar motor %motor"
+    //% group='Carrinho' weight=0 blockGap=8
+    export function carMotorStop(motor: EscolaCarMotor) {
+        if (motor==EscolaCarMotor.MotorD){
+            pins.digitalWritePin(DigitalPin.P0, 1)
+            pins.digitalWritePin(DigitalPin.P1, 1)
+            pins.analogWritePin(AnalogPin.P8, 1023)
+        } else {
+            pins.digitalWritePin(DigitalPin.P2, 1)
+            pins.digitalWritePin(DigitalPin.P13, 1)
+            pins.analogWritePin(AnalogPin.P15, 1023)
+        }
     }
 }
